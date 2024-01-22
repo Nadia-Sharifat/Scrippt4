6f9a5a1d53a1c8c025935825dba26a728028aaa3
Nadia Sharifat <nadia@outlook.com>
2024-01-22 22:43:28 +0330

    version 1.1

---
+++ script4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Enter a number: " NUMBER
+if [ $NUMBER -eq 10 ]
+then
+	echo "It is Equal to 10"
+elif [ $NUMBER -lt 10 ]
+then
+	echo "IT is less than 10"
+else
+	echo "IT is greater than 10"
+fi
+
